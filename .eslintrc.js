ba13c826a8b12411071f996d3d7a0b73575758cc
Ayushi Dangayach <ayushi.dangayach@dianapps.com>
2022-04-28 07:46:55 +0530

    lint test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,28 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        amd: true,
-        node: true,
+  env: {
+    browser: true,
+    es2021: true,
+    amd: true,
+    node: true,
+  },
+  extends: ['plugin:react/recommended', 'eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-        'plugin:react/recommended',
-        'eslint:recommended', 
-        'plugin:prettier/recommended'
-    ],
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 'latest',
-        sourceType: 'module',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['prettier', 'react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'no-return-assign': ['off'],
+    'react/prop-types': ['warn'],
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    plugins: ['prettier', 'react'],
-    rules: {
-        'react/react-in-jsx-scope': 'off',
-        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-        'no-return-assign': ['off'],
-        'react/prop-types': ['warn'],
-    },
+  },
 };
