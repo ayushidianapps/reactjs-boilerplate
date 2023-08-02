0bda17078784e6f38d05eb1b7e9ed62e26a623be
Ayushi Dangayach <ayushi.dangayach@dianapps.com>
2023-08-02 17:37:11 +0530

    context provider example

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
+import { useContext } from 'react';
+import { ThemeContext } from './context/ThemeContext';
+
 function App() {
+  const { isThemeDark, setIsThemeDark } = useContext(ThemeContext);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -6,6 +11,10 @@ function App() {
           You are running this on {process.env.NODE_ENV} and the URL to it is <br />
           {process.env.REACT_APP_URL}
         </p>
+        <div>
+          Using the <strong>{isThemeDark ? 'Dark' : 'Light'}</strong> theme. Do you wanna switch?{' '}
+          <input type="checkbox" onChange={() => setIsThemeDark((prev) => !prev)} />
+        </div>
       </header>
     </div>
   );
